ef857eb0e746477d55f6beebecec530a7f9b5589
AlexCurtaud <alex.curtaud@gmail.com>
2026-03-05 17:27:29 +0100

    Exception Package - Delete OutOfBoardException - Not used, just a test

--- src/Exception/OutOfBoardException.java
+++
@@ -1,7 +0,0 @@
-package Exception;
-
-public class OutOfBoardException extends Exception {
-    public OutOfBoardException() {
-        System.out.println("You've reached the end of the board ! Well done, you win !");
-    }
-}
