1d107d4b484b83f872760bc4901631fa66241ba0
AlexCurtaud <alex.curtaud@gmail.com>
2026-03-12 13:38:36 +0100

    Exception : InvalidPlayer - Update with two constructor - 1 construct with this("")

--- src/Exception/PlayerTypeInvalidException.java
+++ src/Exception/PlayerTypeInvalidException.java
@@ -1,7 +1,11 @@
 package Exception;
 
 public class PlayerTypeInvalidException extends Exception {
+    public PlayerTypeInvalidException(String message) {
+        super("The Character class you're trying to select doesn't exist." + message);
+    }
+    // this("") call the previous constructor with a null parameter
     public PlayerTypeInvalidException() {
-        super("The Character class you're trying to select doesn't exist.");
+        this("");
     }
 }
